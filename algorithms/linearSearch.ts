46314dd6c8f3d182bead041007024df38df5d631
hanna.serozhko <serozhkohanna@gmail.com>
2020-07-17 11:10:45 +0300

    Linear Search.

---
+++ algorithms/linearSearch.ts
@@ -0,0 +1,24 @@
+class LinearSearch {
+  array: string[];
+
+  constructor(data) {
+	this.array = data;
+  }
+
+  indexOf(target: number | string): (number | string) {
+	const array = this.array ? this.array : [];
+
+	let maxItems = array.length;
+	let startItem = 0;
+
+	for (startItem; startItem < maxItems; startItem++) {
+	  if (array[startItem] == target) {
+		return startItem;
+	  }
+	}
+	return 'Not Found';
+  }
+}
+
+const SEARCH_LINEAR = new LinearSearch(['monday', 'tuesday', 'wednesday', 'thur', 'fr'])
+console.log(SEARCH_LINEAR.indexOf('wednesdayfd'));
