c9fef9a1866033be1a89368a0239a99e5c705b6d
hanna.serozhko <serozhkohanna@gmail.com>
2020-07-17 10:48:56 +0300

    Binary SEARCH > improvements.

--- algorithms/binarySearch.ts
+++ algorithms/binarySearch.ts
@@ -1,29 +1,32 @@
 class BinarySearch {
-  array: number[]
+  array: number[];
 
   constructor(numbers: number[]) {
 	const sortedArray = [...numbers].sort((a, b) => a - b)
-	this.array = numbers;
+	this.array = sortedArray;
   }
 
   indexOf(target: number) {
-	const array = this.array ? this.array : []
-	let low = 0
-	let high = array.length
-	while (low <= high) {
-	  const mid = Math.floor((low + high) / 2)
-	  if (array[mid] === target) {
-		return mid
-	  } else if (array[mid] < target) {
-		low = mid + 1
+	const array = this.array ? this.array : [];
+
+	let startItem = 0
+	let maxItems = array.length
+
+	while (startItem <= maxItems) {
+	  const middleValue= Math.floor((startItem + maxItems) / 2)
+
+	  if (array[middleValue] === target) {
+		return middleValue
+	  } else if (array[middleValue] < target) {
+		startItem = middleValue + 1
 	  } else {
-		high = mid - 1
+		maxItems = middleValue - 1
 	  }
 	}
-	return -1
+	return 'Not found'
   }
 }
 
 const SEARCH = new BinarySearch([1, 3, 44, 5, 6, 3]);
-console.log(SEARCH)
-console.log(SEARCH.indexOf(5))
+
+console.log(SEARCH.indexOf(6));
