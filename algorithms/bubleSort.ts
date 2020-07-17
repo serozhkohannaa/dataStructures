ff74ba16d69fb964ddcc24958d8e9aa4515cf96d
hanna.serozhko <serozhkohanna@gmail.com>
2020-07-17 12:35:53 +0300

    Bubble sort.

--- algorithms/bubleSort.ts
+++ algorithms/bubleSort.ts
@@ -1,17 +1,27 @@
-function bubbleSort(array: number[]): number[] {
-  array = array.slice(); // creates a copy of the array
+class bubbleSort {
+  array: number[];
 
-  for(let i = 0; i < array.length; i++) {
-	for(let j = 0; j < array.length - 1; j++) {
+  constructor(numbers: number[]) {
+	this.array = [...numbers];
+  }
 
-	  if(array[j] > array[j + 1]) {
-		let swap = array[j];
-		array[j] = array[j + 1];
-		array[j + 1] = swap;
+  isSort() {
+    let startItem = 0;
+    let maxItems = this.array.length;
+
+	for (startItem; startItem < maxItems; startItem++) {
+	  for (let j = 0; j < maxItems - 1; j++) {
+
+		if (this.array[j] > this.array[j + 1]) {
+		  let swap = this.array[j];
+		  this.array[j] = this.array[j + 1];
+		  this.array[j + 1] = swap;
+		}
 	  }
 	}
+	return this.array;
   }
-  return array;
 }
 
-console.log(bubbleSort([1,3,4,5,2,4,5,6,3]))
+const SORT = new bubbleSort([1, 100, 45, 3, 5, 7, 8, 5, 4, 3, -5]);
+console.log(SORT.isSort())
